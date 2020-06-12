151fe0cd26030f23a2f52d7aae6dc3a506d2c44f
Bill Ruediger <ruediger@uw.edu>
2020-06-12 14:00:53 -0700

    Implemented Multiply

--- CustomerAPI/Models/Customer.cs
+++ CustomerAPI/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,30 @@ namespace CustomerAPI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public int AgeInMonths { get; }
+        [NotMapped]
+        public int AgeInMonths { 
+            get 
+            { 
+                return Multiply(Age, 12); 
+            } 
+        }
+
+        /// <summary>
+        /// Multiple x times y without using built in multiply operator.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int Multiply(int x, int y)
+        {
+            int result = 0;
+
+            for(int iteration=0;iteration<y;iteration++)
+            {
+                result = result + x;
+            }
+
+            return result;
+        }
     }
 }
