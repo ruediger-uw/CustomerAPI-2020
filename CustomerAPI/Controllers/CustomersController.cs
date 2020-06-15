0610efa4fa412a1fac7169479f8d00d1745d9cc1
Bill Ruediger <ruediger@uw.edu>
2020-06-15 15:00:24 -0700

    Downgraded to .NET Core 2.1

--- CustomerAPI/Controllers/CustomersController.cs
+++ CustomerAPI/Controllers/CustomersController.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CustomerAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerAPI.Controllers
 {
--- CustomerAPI/Startup.cs
+++ CustomerAPI/Startup.cs
@@ -8,10 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using CustomerAPI.Models;
+
 
 namespace CustomerAPI
 {
@@ -28,27 +28,23 @@ namespace CustomerAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("CustomerList"));
-            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseMvc();
         }
     }
 }
